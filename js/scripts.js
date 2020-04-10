6834c901f0e64f1410bff7559ac39110387546f6
Ryan Duff <ryanduff613@gmail.com>
2020-04-10 11:06:55 -0700

    create addCustomerSpecs method on Pizza prototype that adds toppings and size info onto Pizza objects

--- js/scripts.js
+++ js/scripts.js
@@ -5,23 +5,29 @@ function Pizza(toppings, size){
   this.price = 0
 }
 
+Pizza.prototype.addCustomerSpecs = function(toppings, size){
+  this.toppings = toppings;
+  this.size = size;
+}
+
 //User Logic --------------------------------------
 $(document).ready(function(){
   $('#theForm').submit(function(event){
     event.preventDefault();
+    var pizza = new Pizza;
     var size = $('#size').val();
     var toppings = [];
     $('input:checkbox[name=topping]:checked').each(function(){
       toppings.push($(this).val());
     })
-    toppings.forEach(function(topping){
-     console.log(topping +', ');
-    });
-    
+    pizza.addCustomerSpecs(toppings, size);
     $('#outputDiv').show();
-    var order = new Pizza()
-    toppings.forEach(function(topping){
-     return topping;
-   });
+    console.log(pizza, size, toppings);
   })
 });
+
+/*
+toppings.forEach(function(topping){
+  console.log(topping +', ');
+ });
+ */
