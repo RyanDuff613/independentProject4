046da1f9e166a672c1cc1e7cca5c800556a8d62d
Ryan Duff <ryanduff613@gmail.com>
2020-04-10 10:10:07 -0700

    create toppings array, push values of checkboxes into array when user submits

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
+// Business logic ---------------------------------
 function Pizza(toppings, size){
   this.toppings = toppings,
   this.size = size,
   this.price = 0
 }
 
-
+//User Logic --------------------------------------
 $(document).ready(function(){
   $('#theForm').submit(function(event){
     event.preventDefault();
+    var toppings = [];
+    $('input:checkbox[name=topping]:checked').each(function(){
+      toppings.push($(this).val());
+      $('#outputDiv').append(toppings);
+      console.log(toppings);
+      $('#outputDiv').show();
+    })
     var order = new Pizza()
   })
 });
